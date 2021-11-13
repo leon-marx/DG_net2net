f9846afe14f71b9a786225b136cf430869661ae6
leon-marx <gooogleon@gmail.com>
2021-11-13 14:38:24 +0100

    changed checkpoint to newest model and implemented metrics_reader.py

---
+++ tools/metrics_reader.py
@@ -0,0 +1,24 @@
+from typing import KeysView
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+log_path = "logs/2021-11-11T20-42-57_pacs_dog/testtube/version_0/metrics.csv"
+
+df = pd.read_csv(log_path)
+# print(df.head())
+# print(df.tail())
+
+print(list(df.keys()))
+
+keys = list(df.keys())
+keys = ["epoch"]
+
+print(df["created_at"])
+
+for i, k in enumerate(keys):
+    plt.figure(figsize=(12, 8))
+    #plt.subplot(4, np.ceil(len(keys)/4), i+1)
+    plt.scatter(df.index, df[k], label=k)
+    plt.legend()
+    plt.show()
