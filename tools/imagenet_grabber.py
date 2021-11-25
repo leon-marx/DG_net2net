08f45591484b86a3fde25e3396af17ee1b5fb323
leon-marx <gooogleon@gmail.com>
2021-11-25 19:09:56 +0100

    added subprocess calling command

--- tools/imagenet_grabber.py
+++ tools/imagenet_grabber.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import subprocess
 
 content_list = ["dog", "elephant", "giraffe", "guitar", "horse", "house", "person"]
 
@@ -11,5 +12,7 @@ for content in content_list:
     for id in ids:
         class_list += id + " "
     # print(class_list)
-    command = f"python ../imagenet-datasets-downloader/downloader.py -data_root /home/../hd-data/pacs_classes_only/{content} -use_class_list True -class_list {class_list}-images_per_class 10000"
-    print(command)
+    bashCommand = f"python ../imagenet-datasets-downloader/downloader.py -data_root /home/../hd-data/pacs_classes_only/{content} -use_class_list True -class_list {class_list}-images_per_class 10000"
+    print(bashCommand)
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
