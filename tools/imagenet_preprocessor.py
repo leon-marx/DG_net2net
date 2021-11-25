cd688e062eae216eff3b6a8cbd70158c218a0300
leon-marx <gooogleon@gmail.com>
2021-11-25 20:46:18 +0100

    implemented datahandling for bigger imagenet

--- tools/imagenet_preprocessor.py
+++ tools/imagenet_preprocessor.py
@@ -7,12 +7,14 @@ train_test_split = 0.8
 
 data = []
 
-for domain_dir in os.listdir("data/imagenet"):
-    print(domain_dir)
-    for content_dir in os.listdir("data/imagenet/" + domain_dir):
-        print("   " + content_dir)
-        for im in os.listdir("data/imagenet/" + domain_dir + "/" + content_dir):
-            data.append([domain_dir, content_dir, im])
+main_dir = "/home/../hd-data/imagenet/pacs_classes_only"
+train_dir = "/home/../hd-data/imagenet/imagenet_train"
+test_dir = "/home/../hd-data/imagenet/imagenet_test"
+
+for content_dir in os.listdir(main_dir):
+    print(content_dir)
+    for im in os.listdir(main_dir + "/" + content_dir):
+        data.append(["photo", content_dir, im])
 
 data = np.array(data)
 np.random.shuffle(data)
@@ -21,26 +23,22 @@ N = data.shape[0]
 train_data = data[:int(N * train_test_split)]
 test_data = data[int(N * train_test_split):]
 
-if not os.path.exists("data/imagenet_train/meta"):
-    os.makedirs("data/imagenet_train/meta")
+if not os.path.exists(train_dir + "/meta"):
+    os.makedirs(train_dir + "/meta")
 for d in train_data:
-    with Image.open("data/imagenet/" + d[0] + "/" + d[1] + "/" + d[2]) as image:
-        resized = T.functional.pad(image, padding=(14, 14, 15, 15), fill=256)
-        if not os.path.exists("data/imagenet_train/" + d[0] + "/" + d[1]):
-            os.makedirs("data/imagenet_train/" + d[0] + "/" + d[1])
-        # resized.save("data/imagenet_train/" + d[0] + "/" + d[1] + "/" + d[2])
-        image.save("data/imagenet_train/" + d[0] + "/" + d[1] + "/" + d[2])
-    with open(f"data/imagenet_train/meta/{d[0]}.txt", "a") as f:
+    with Image.open(main_dir + "/" + d[1] + "/" + d[2]) as image:
+        if not os.path.exists(train_dir + "/photo/" + d[1]):
+            os.makedirs(train_dir + "/photo/" + d[1])
+        image.save(train_dir + "/photo/" + d[1] + "/" + d[2])
+    with open(f"{train_dir}/meta/photo.txt", "a") as f:
         f.write(d[1] + "/" + d[2] + "\n")
 
-if not os.path.exists("data/imagenet_test/meta"):
-    os.makedirs("data/imagenet_test/meta")
+if not os.path.exists(test_dir + "/meta"):
+    os.makedirs(test_dir + "/meta")
 for d in test_data:
-    with Image.open("data/imagenet/" + d[0] + "/" + d[1] + "/" + d[2]) as image:
-        resized = T.functional.pad(image, padding=(14, 14, 15, 15), fill=256)
-        if not os.path.exists("data/imagenet_test/" + d[0] + "/" + d[1]):
-            os.makedirs("data/imagenet_test/" + d[0] + "/" + d[1])
-        # resized.save("data/imagenet_test/" + d[0] + "/" + d[1] + "/" + d[2])
-        image.save("data/imagenet_test/" + d[0] + "/" + d[1] + "/" + d[2])
-    with open(f"data/imagenet_test/meta/{d[0]}.txt", "a") as f:
+    with Image.open(main_dir + "/" + d[1] + "/" + d[2]) as image:
+        if not os.path.exists(test_dir + "/photo/" + d[1]):
+            os.makedirs(test_dir + "/photo/" + d[1])
+        image.save(test_dir + "/photo/" + d[1] + "/" + d[2])
+    with open(f"{test_dir}/meta/photo.txt", "a") as f:
         f.write(d[1] + "/" + d[2] + "\n")
